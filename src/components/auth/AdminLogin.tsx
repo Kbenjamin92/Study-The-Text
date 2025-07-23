d3c743d0d9464ebab5014a10f5266d3b5343f7d2
Kbenjamin92 <kipplbenjamin@gmail.com>
2025-07-23 09:55:27 -0400

    added react-hook-forms to the admin login form

--- src/components/auth/AdminLogin.tsx
+++ src/components/auth/AdminLogin.tsx
@@ -4,7 +4,12 @@ import {
 import { Toaster } from "../../components/ui/toaster"
 import { PasswordInput } from "../../components/ui/password-input"
 import { useAuth } from '../../hooks/useAuth';
+import { useForm } from "react-hook-form"
+import type { SubmitHandler } from "react-hook-form";
+import type { AdminContextType } from '../../interface';
 
+
+//  added the react hook forms 
 export const AdminLogin = () => {
 
 const context = useAuth();
@@ -17,26 +22,51 @@ const context = useAuth();
     setPassword, 
     handleLogin } = context;
 
+    const {
+    register,
+    handleSubmit
+  } = useForm<AdminContextType>()
+
+  const onSubmit: SubmitHandler<AdminContextType> = (data) => {
+    console.log(data);
+    handleLogin();
+  }
   return (
-    <Box maxW="md" mx="auto" mt={10} p={6} boxShadow="md" borderRadius="md">
-        <VStack align="stretch">
-            <Field.Root>
-                <Field.Label fontSize='lg' paddingBottom='15px'>Admin Login</Field.Label>
-                <Field.Label paddingBottom='10px' fontWeight='light'>Login to access the dashboard and upload files.</Field.Label>
-            </Field.Root>
-         </VStack>
-      <VStack>
-        <Field.Root>
-          <Field.Label>Username</Field.Label>
-          <Input value={username} onChange={(e) => setUsername(e.target.value)} />
-        </Field.Root>
-        <Field.Root>
-          <Field.Label>Password</Field.Label>
-            <PasswordInput type="password" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
-        </Field.Root>
-        <Button onClick={handleLogin} colorScheme="brown">Login</Button>
-      </VStack>
-      <Toaster />
-    </Box>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Box maxW="md" mx="auto" mt={10} p={6} boxShadow="md" borderRadius="md">
+          <VStack align="stretch">
+              <Field.Root>
+                  <Field.Label fontSize='lg' paddingBottom='15px'>Admin Login</Field.Label>
+                  <Field.Label paddingBottom='10px' fontWeight='light'>Login to access the dashboard and upload files.</Field.Label>
+              </Field.Root>
+          </VStack>
+          <VStack>
+          <Field.Root>
+            <Field.Label>Username</Field.Label>
+            <Input 
+              {...register("username", { required: true })}
+              type="text"
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)} />
+          </Field.Root>
+          <Field.Root>
+            <Field.Label>Password</Field.Label>
+              <PasswordInput 
+                {...register("password", { required: true })}
+                type="password" 
+                value={password} 
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
+          </Field.Root>
+          <Button 
+            type='submit' 
+            width='100%' 
+            marginTop='10px' 
+            bgColor="black">Login</Button>
+        </VStack>
+        <Toaster />
+      </Box>
+    </form>
+    </>
   );
 }
